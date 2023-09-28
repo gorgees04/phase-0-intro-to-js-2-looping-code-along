92382a3978adf5cb563b55d5fe715048f1a4c870
Gorgees Odisho <gorgees041@gmail.com>
2023-09-28 22:46:58 +0300

    FOR AND WHILE LOOP DONE

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    const msgs = []
+    for (let i = 0; i < names.length; i++) {
+        msgs.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return msgs
+}
+
+function countDown(n) {
+    const count = []
+    let i = n + 1
+    while (i > 0) {
+        i--
+        console.log(i)
+    }
+    return count
+}
+
+
+
